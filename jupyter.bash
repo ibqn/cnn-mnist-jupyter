84c611ec25bb3632030f1ded77027b11b9f5bd8f
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-08-01 07:42:43 +0200

    switch to jupyterlab. update deps

--- jupyter.bash
+++ jupyter.bash
@@ -9,7 +9,7 @@ source './venv/bin/activate'
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade --requirement './requirements.txt'
 
-exec jupyter notebook # --no-browser --port 9999
+exec jupyter lab --no-browser --port 9999
 
 # you might need to install
 # sudo apt install python3-venv
